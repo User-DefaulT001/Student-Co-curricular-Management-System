416903d63e1f359a39733a1c58116a4352e7cc86
ImHang373 <kaihang8082@gmail.com>
2026-04-13 02:29:09 +0800

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/Student-Co-curricular-Management-System-main/index.php">
+        <div class="sidebar-brand-text mx-3"><i class="fas fa-graduation-cap"></i> Student CMS</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+    <li class="nav-item">
+        <a class="nav-link" href="/Student-Co-curricular-Management-System-main/index.php"><i class="fas fa-fw fa-tachometer-alt"></i><span>Dashboard</span></a>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">Modules</div>
+    
+    <li class="nav-item">
+        <a class="nav-link" href="/Student-Co-curricular-Management-System-main/modules/event/event_tracker.php"><i class="fas fa-fw fa-calendar"></i><span>Event Tracker</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#" onclick="alert('Coming Soon!'); return false;" style="opacity: 0.6;"><i class="fas fa-fw fa-users"></i><span>Club Tracker</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/Student-Co-curricular-Management-System-main/modules/merit/merit_tracker.php"><i class="fas fa-fw fa-star"></i><span>Merit Tracker</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="/Student-Co-curricular-Management-System-main/modules/achievement/achievement_tracker.php"><i class="fas fa-fw fa-trophy"></i><span>Achievement Tracker</span></a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
+<div id="content-wrapper" class="d-flex flex-column">
